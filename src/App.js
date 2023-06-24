b52dc0a15c6d7f1b4189f3782bf714efd810fb98
Souvik Nandi <souviknandi707@gmail.com>
2023-06-24 20:39:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,53 @@
+
+import './App.css';
+
+import React, { useState } from 'react';
+
+import Tours from './components/Tours';
+
+import data from './data'
+
+
+const App=()=> {
+
+
+const[tours,setTours]=useState(data);
+
+
+function removeTour(id){
+    
+const newTours=tours.filter(tour=>tour.id !==id);
+setTours(newTours);
+
+}
+
+if(tours.length===0){
+  return(
+
+<div className='refresh'>
+<h2>No Tours Left</h2>
+<button className='btn-white' onClick={()=>setTours(data)}>Refresh</button>
+</div>
+
+  );
+}
+
+
+
+  return (
+    <div className="App">
+    <div>
+
+
+
+   <Tours tours={tours} removeTour={removeTour} ></Tours>   
+
+
+
+
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,105 @@
+
+
+const data=[
+
+
+{
+
+id:1,
+name:"Munnar",
+info:"Munnar, Enjoying an elevation of 2,100 metres above the sea level, this peak is as mesmerising as you can expect it to be. Offering a short and convenient trek which can be easily completed in a day's time, this hill houses the exemplary Shoals Forest.Virgin forests, savannah, rolling hills, scenic valleys, numerous streams, huge splashy waterfalls, sprawling tea plantations and winding walkways are all part of the great holiday experience on offer for a traveler to Munnar. Munnar is also known for Neelakurinji, a rare plant which flowers only once in twelve years....",
+image:"https://www.holidify.com/images/cmsuploads/compressed/104300123_20210820180535.jpg",
+price:"32,068",
+
+
+},
+{
+    id:2,
+    name:"Delhi",
+    info:"Delhi had been the hub of the Mughal dynasty, and many historical monuments were built in Delhi. The Red Fort is one of the structures which signify the Mughal power and their dynasty. It was built by Emperor Shah Jahan in 1648. It is made up of red stones, and therefore it is named Red Fort. It is known for its beautiful architecture. Check the samples provided below to learn more about the Red Fort and how to write a paragraph on the same....",
+    image:"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/10/3c/4a/42/fb-img-1502472371491.jpg?w=1000&h=-1&s=1",
+    price:"30,623",
+    
+},
+
+
+{
+
+    id:3,
+    name:"Gangtok",
+    info:"Gangtok is the capital of the mountainous northern Indian state of Sikkim. Established as a Buddhist pilgrimage site in the 1840s, the city became capital of an independent monarchy after British rule ended, but joined India in 1975. Today, it remains a Tibetan Buddhist center and a base for hikers organizing permits and transport for treks through Sikkim’s Himalayan mountain ranges....",
+    image:"https://etripto.in/uploads/0000/1/2022/12/18/sikkim-tourism-4.jpeg",
+    price:"27,630",
+    
+    
+    },
+    {
+        id:4,
+        name:"Andaman and Nicobar Islands",
+        info:"The Andaman Islands are an Indian archipelago in the Bay of Bengal. These roughly 300 islands are known for their palm-lined, white-sand beaches, mangroves and tropical rainforests. Coral reefs supporting marine life such as sharks and rays make for popular diving and snorkeling sites. Indigenous Andaman Islanders inhabit the more remote islands, many of which are off limits to visitors....",
+        image:"https://www.travelogyindia.com/storage/app/itinerary/504/corbyn-cove-beach-andaman.jpg",
+        price:"39,122",
+        
+    },
+    
+    
+    {
+
+        id:5,
+        name:"Manali",
+        info:"Manali is a high-altitude Himalayan resort town in India’s northern Himachal Pradesh state. It has a reputation as a backpacking center and honeymoon destination. Set on the Beas River, it’s a gateway for skiing in the Solang Valley and trekking in Parvati Valley. It's also a jumping-off point for paragliding, rafting and mountaineering in the Pir Panjal mountains, home to 4,000m-high Rohtang Pass....",
+        image:"https://pix6.agoda.net/hotelImages/31586238/-1/2ca84202c25f18bac895e0bb14e6e6bf.jpg",
+        price:"14,447",
+        
+        
+        },
+        {
+            id:6,
+            name:"Santiniketan",
+            info:"Santiniketan, popularly known today as a university town, a hundred miles to the north of Kolkata, was originally an ashram built by Debendranath Tagore, where anyone, irrespective of caste and creed, could come and spend time meditating on the one Supreme God.Ideally the best time to visit any place is considered when the weather conditions are delightful. For Shantiniketan, you can choose to go in October, November, December, January and February which is the peak season and July, August and September which is the moderate season....",
+            image:"https://upload.wikimedia.org/wikipedia/commons/e/ed/Shantiniketan_Bari_of_Rabindranath_Tagore.jpg",
+            price:"5000",
+            
+        },
+        
+        
+        {
+
+            id:7,
+            name:"Switzerland",
+            info:"Switzerland is famous for its lakes, rivers, mountains, and natural beauty. It is also known for its diversity of languages ​​and religions. Switzerland is the only country with four national languages ​​and one of those countries where you can go at any time and enjoy it in the best way.One of the most beautiful villages in Switzerland is Grindelwald. What makes this village special is that it is a place that is surrounded by very tall mountains on two sides. Furthermore, there are many viewpoints in Grindelwald from where you can see amazing landscapes that are stunning....",
+            image:"https://live.staticflickr.com/3690/11957350796_283fbdf9a1_b.jpg",
+            price:"80,995",
+            
+            
+            },
+            {
+                id:8,
+                name:"Rajasthan",
+                info:"Rajasthan is a state in northern India. It covers 342,239 square kilometres or 10.4 per cent of India's total geographical area. It is the largest Indian state by area and the seventh largest by population.Rajasthan is known for its historical hill forts & palaces, it is claimed as the best place for tourism-related to palaces. Umaid Bhawan Palace: It is the largest Royal Palace in Rajasthan. It is also one of the largest private residences in the world....",
+                image:"https://www.holidify.com/images/bgImages/DEEG.jpg",
+                price:"35,990",
+                
+            },
+            {
+                id:9,
+                name:"Maldives",
+                info:"The Maldives, officially the Republic of Maldives, is an archipelagic state and country in South Asia, situated in the Indian Ocean. It lies southwest of Sri Lanka and India, about 750 kilometres from the Asian continent's mainland.The Maldives is world-famous for its myriad of marine life, and is considered to be one of the best snorkelling and diving locations in the world. Swim freely in the Indian Ocean among the colourful arrays of corals ....",
+                image:"https://assets.traveltriangle.com/blog/wp-content/uploads/2015/06/Maldives-Honeymoon.jpg",
+                price:"63,970",
+                
+            },
+            {
+                id:9,
+                name:"Mandarmani",
+                info:"It is almost 172 km from Kolkata Airport on the Kolkata - Digha route. Red crabs crawling around the 13 km long beach is a special attraction of Mandarmani. It was argued to be the longest driveable (drive in) beach in India. It is located under Contai Sub division area.The beauty of the place during winter season is something to be enjoyed. The lowest temperature at the place is about 10° C. The maximum temperature at the place is 28°C. ....",
+                image:"https://live.staticflickr.com/65535/49915283396_ff1bf51f27_b.jpg",
+                price:"15,970",
+                
+            },
+            
+           
+            
+];
+
+export default data;
